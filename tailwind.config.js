1a157800a3af6d1870dde8d4e421d254d0cb6818
Kazuma Kurisaki <kazuma@Kazumas-MacBook-Pro.local>
2022-03-26 15:53:38 +0900

    set up next ts tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'jit',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+};
