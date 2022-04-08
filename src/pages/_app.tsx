35952f7d16007f936cbb160b43f25d3bde497958
Kazuma Kurisaki <kazuma@Kazumas-MacBook-Pro.local>
2022-04-08 19:44:08 +0900

    SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,34 @@
 import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
+import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo
+        defaultTitle='VB Music'
+        canonical='' //ここにurl
+        description='バーチャルおばあちゃんの三味線ライブの曲を検索！'
+        openGraph={{
+          type: 'website',
+          title: 'VB Music',
+          description: 'バーチャルおばあちゃんの三味線ライブの曲を検索',
+          site_name: 'VB Music',
+          url: '',
+          images: [
+            {
+              url: '/image/main/home.JPG',
+              width: 800,
+              height: 600,
+              alt: 'VB Music',
+            },
+          ],
+        }}
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
